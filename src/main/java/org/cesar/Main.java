b5001aa441b14bc73f72a74eb29dd8301887e96f
NotMacaco22 <casilva@uamv.edu.ni>
2024-10-08 17:56:43 -0600

    First Commit

---
+++ src/main/java/org/cesar/Main.java
@@ -0,0 +1,11 @@
+package org.cesar;
+
+public class Main {
+    /*
+    * id (Long, autogenerado)
+        nombreCompleto (String)
+        email (String)
+        telefono (String)
+        fechaRegistro (LocalDateTime)
+    * */
+}
