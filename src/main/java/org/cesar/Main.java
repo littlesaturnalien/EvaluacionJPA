ef8d228e31d65b5ef2106761c58a4d13f7c862b0
NotMacaco22 <casilva@uamv.edu.ni>
2024-10-08 18:17:01 -0600

    other Commit

--- src/main/java/org/cesar/Main.java
+++ src/main/java/org/cesar/Main.java
@@ -1,11 +1,36 @@
 package org.cesar;
 
+import org.cesar.dao.ClientDAO;
+
+import java.util.Scanner;
+
 public class Main {
-    /*
-    * id (Long, autogenerado)
-        nombreCompleto (String)
-        email (String)
-        telefono (String)
-        fechaRegistro (LocalDateTime)
-    * */
+    public static void main(String[] args) {
+
+        ClientDAO clientDAO = new ClientDAO();
+        Scanner scanner = new Scanner(System.in);
+
+        int opc = 0;
+
+        while(opc != 5){
+            System.out.println("~ Client Database ~\n1. Add New Client\n2. Delete Client\n3. Update Client\n4. Show All Clients\n5. Exit");
+            System.out.print("Select An Option: ");
+            opc = scanner.nextInt();
+
+            switch(opc){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+
+
+        }
+    }
 }
